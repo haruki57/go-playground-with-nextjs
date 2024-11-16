cc7e7dc7c3165fb161fd2ed69e7c153e741323c6
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-11-16 14:21:00 +0900

    Made it possible to specify room id

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,43 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
+  const [room, setRoom] = useState<string>("");
   const [messages, setMessages] = useState<string[]>([]);
-
-  useEffect(() => {
-    const ws = new WebSocket(`${process.env.NEXT_PUBLIC_BACKEND_URL}/ws/124`);
-    ws.onopen = () => {
-      console.log("Connected to room:123");
+  const [ws, setWs] = useState<WebSocket | undefined>();
+  const onChangeRoom = (e: ChangeEvent<HTMLInputElement>) => {
+    setRoom(e.target.value);
+  };
+  const applyRoomChange = () => {
+    if (ws) {
+      ws.close();
+    }
+    if (room.length == 0) {
+      return;
+    }
+    const newWs = new WebSocket(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/ws/${room}`
+    );
+    newWs.onopen = () => {
+      console.log("Connected to room:" + room);
     };
-
-    ws.onclose = () => {
+    newWs.onclose = () => {
       console.log("Disconnected from WebSocket");
     };
-    ws.onmessage = (event) => {
+    newWs.onmessage = (event) => {
       setMessages((prev) => [...prev, event.data]);
     };
     setInterval(() => {
-      ws.send(new Date().toISOString());
+      newWs.send(new Date().toISOString());
     }, 1000);
-  }, []);
+    setWs(newWs);
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <input type="text" value={room} onChange={onChangeRoom}></input>
+      <input type="button" value="変更" onClick={applyRoomChange}></input>
+      <div>{`RoomId: ${room}`}</div>
       {JSON.stringify(messages)}
     </div>
   );
